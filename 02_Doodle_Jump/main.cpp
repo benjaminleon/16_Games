5b612e0661b9edefa286d31b1bcfb965371b337d
ben <benjamin.lind@cybercom.com>
2017-04-25 11:21:16 +0200

    Introduce boosting platforms

--- 02_Doodle_Jump/main.cpp
+++ 02_Doodle_Jump/main.cpp
@@ -12,8 +12,8 @@ struct point
 
 enum platformType
 {
-    strong,
     weak,
+    strong,
     boost
 };
 
@@ -26,18 +26,19 @@ int main()
     RenderWindow app(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Doodle Game!");
     app.setFramerateLimit(60);
 
-    Texture t1, t2, t3, t4;
+    Texture t1, t2, t3, t4, t5;
     t1.loadFromFile("images/background.png");
     t2.loadFromFile("images/platform.png");
     t3.loadFromFile("images/doodle.png");
     t4.loadFromFile("images/platform_yellow.png");
+    t5.loadFromFile("images/platform_boost.png");
 
     const int DIST_TO_RIGHT_FOOT = 50;
     const int DIST_TO_LEFT_FOOT = 20;
 
-    Sprite sBackground(t1), sPlat(t2), sPers(t3), sPlatWeak(t4);
+    Sprite sBackground(t1), sPlat(t2), sPers(t3), sPlatWeak(t4), sPlatBoost(t5);
 
-    const int NR_OF_PLATFORMS = 10;
+    const int NR_OF_PLATFORMS = 5;
     point plat[NR_OF_PLATFORMS];
 
     // Initialize platforms
@@ -45,7 +46,7 @@ int main()
     {
         plat[i].x = rand() % SCREEN_WIDTH;
         plat[i].y = rand() % SCREEN_HEIGHT;
-        plat[i].mode = (rand() % 2 > 0) ? platformType::weak : platformType::strong;
+        plat[i].mode = rand() % 3;
     }
 
     int x = 100, y = SCREEN_HEIGHT, moveHigherThreshold = SCREEN_HEIGHT * 2 / 3;
@@ -94,34 +95,52 @@ int main()
                 }
             }
 
-        for (int i = 0; i < NR_OF_PLATFORMS; i++) // Bounce
-            if ((x + DIST_TO_RIGHT_FOOT > plat[i].x) && (x + DIST_TO_LEFT_FOOT < plat[i].x + 68) && (y - 70 > plat[i].y) && (y - 70 < plat[i].y + 14) && falling)
+        int bouncingPlatform;
+        for (int i = 0; i < NR_OF_PLATFORMS; i++)
+            if ((x + DIST_TO_RIGHT_FOOT > plat[i].x) && (x + DIST_TO_LEFT_FOOT < plat[i].x + 68) && (y - 70 > plat[i].y) && (y - 70 < plat[i].y + 14)) // touching a platform
             {
-                ySpeed = 10;
-                
-                if (plat[i].mode == platformType::weak)
+                bouncingPlatform = i;
+                if (falling)
                 {
-                    plat[i].x = rand() % SCREEN_WIDTH;
-                    plat[i].x = SCREEN_HEIGHT;
+                    if (plat[i].mode != platformType::boost)
+                        {
+                            ySpeed = 10;
+
+                            if (plat[i].mode == platformType::weak)
+                            {
+                                plat[i].x = rand() % SCREEN_WIDTH;
+                                plat[i].x = SCREEN_HEIGHT;
+                            }
+                        }
+                }
+                else
+                {
+                    if (plat[i].mode == platformType::boost)
+                        ySpeed = 10;
                 }
             }
 
-        sPers.setPosition(x, SCREEN_HEIGHT - y); // (0, 0) is at top left
+        sPers.setPosition(x, SCREEN_HEIGHT - y); // (0, 0) is at top left in sfml. We're using bottom left as (0, 0) in this game
 
         app.draw(sBackground);
         app.draw(sPers);
         for (int i = 0; i < NR_OF_PLATFORMS; i++)
         {
-            if (plat[i].mode == platformType::weak)
+            int type = plat[i].mode;
+            if (type == platformType::weak)
             {
                 sPlatWeak.setPosition(plat[i].x, SCREEN_HEIGHT - plat[i].y);
                 app.draw(sPlatWeak);
             }
-
-            else if (plat[i].mode == platformType::strong)
+            else if (type == platformType::strong)
             {
                 sPlat.setPosition(plat[i].x, SCREEN_HEIGHT - plat[i].y);
                 app.draw(sPlat);
+            }
+            else if (type == platformType::boost)
+            {
+                sPlatBoost.setPosition(plat[i].x, SCREEN_HEIGHT - plat[i].y);
+                app.draw(sPlatBoost);
             }
         }
 
